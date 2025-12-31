23e487e4cafc55db0f5d95f27bd5b3ae55ed3e2d
Kathleen Santos <kathleen.santos@csacademy.com.br>
2025-12-30 23:32:48 -0300

    choice: adjusting validation environments

--- env.ts
+++ env.ts
@@ -1,32 +1,15 @@
-import z, { ZodError } from "zod";
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
-export const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
-  NEXT_PUBLIC_RALI_API_URL : z.url()
-})
-
-const processEnv = {
-  NODE_ENV: process.env.NODE_ENV,
-  NEXT_PUBLIC_RALI_API_URL: process.env.NEXT_PUBLIC_RALI_API_URL
-}
-
-export type Env = z.infer<typeof envSchema>
-
-let env: Env;
-
-try{
-  env = envSchema.parse(processEnv)
-}catch(e){
-  if(e instanceof ZodError){
-    throw new Error(`${e.message}: validation failed`)
-  }
-  throw e;
-}
-
-export { env };
-
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv extends Env {}
-  }
-}
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(['development', 'production', 'test']).default('development')
+  },
+  client: {
+    NEXT_PUBLIC_RALI_API_URL: z.url(),
+  },
+  runtimeEnv: {
+    NEXT_PUBLIC_RALI_API_URL: process.env.NEXT_PUBLIC_RALI_API_URL,
+    NODE_ENV: process.env.NODE_ENV
+  },
+});
