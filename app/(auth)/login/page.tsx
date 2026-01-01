3036f40614c3bbb36c90af7f73e2bad496f84e1d
Kathleen Santos <kathleen.santos@csacademy.com.br>
2026-01-01 20:04:53 -0300

    fix: login error captured in the ranking route, adding middleware to captute cookies in all necessary routes

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -35,7 +35,7 @@ export default async function LoginPage(){
       maxAge: timeExpiresCookie
     })
 
-    redirect('/ranking')
+    redirect('/')
   }
 
   return(
---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('session')?.value;
+  const { pathname } = request.nextUrl;
+
+  const publicRoutes = ['/login'];
+  if (publicRoutes.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+};
